6ba44c567f473050f394ca0ce17581b59258f305
Flaperon09 <kasyanenko.yuriy@gmail.com>
2024-02-18 21:58:45 +0200

    Выполнено ДЗ 6 по JS

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,23 @@
 const textInput = document.querySelector('#validation-input'); // Ищем поле ввода
-let symbols = '';
+let symbols = ''; // Переменная для введённого пароля
 
+// Обработчик событий
 textInput.addEventListener("input", (event) => {
-    symbols = event.currentTarget.value;
-    console.log(symbols);
+    symbols = event.currentTarget.value; // Присвоение введённого пароля переменной
 });
 
 textInput.addEventListener("blur", () => {
-    const arraySymbols = symbols.split('');
-    console.log(arraySymbols);
+    const arraySymbols = symbols.split(''); // Создание массива из букв пароля
+
     if (arraySymbols.length === 0) {
-    textInput.id = 'validation-input';
+        textInput.classList.remove('valid');
+        textInput.classList.remove('invalid');
+        textInput.classList.add('validation-input');
     } else if (arraySymbols.length <= textInput.getAttribute('data-length')) {
-        textInput.id = 'validation-input valid';
+        textInput.classList.remove('invalid');
+        textInput.classList.add('valid');
     } else {
-        textInput.id = 'validation-input invalid';
+        textInput.classList.remove('valid');
+        textInput.classList.add('invalid');
     };
-    
 });
-console.log(textInput);
-console.log(textInput.getAttribute('data-length'));
--- js/task-10.js
+++ js/task-10.js
@@ -4,15 +4,49 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const userControls = document.querySelector('#controls');
-const setObject = document.querySelector('#boxes');
+let startWidth = 30; // Начальная ширина объекта
+let startHight = 30; // Начальная высота объекта
+let amount = 0; // Начальное количество объектов
 
-// Функция создания боксов
-const boxes = createBoxes(amount) {
-  
+const button = document.querySelectorAll('button'); // Формирование массива кнопок
+const number = document.querySelector('input');     // Формирование поля ввода
+const boxes = document.querySelector('#boxes');     // Формирование начального бокса
+
+// Определение введённого числа количества элементов
+number.addEventListener("input", (event) => {
+  amount = event.currentTarget.value
+});
+
+// Логика работы кнопок
+const handleClick = (event) => {
+  if (event.target.outerText === 'Create') { // Если нажата кнопка "Create"
+    // boxes.insertAdjacentHTML("beforeend", "<div></div>");
+      setBoxes(amount); // Вызов функции создания боксов
+    } else if (event.target.outerText === 'Destroy') {  // Иначе (нажата кнопка "Destroy")
+      deleteBoxes();    // Вызов функции удаления боксов  
+    };
 };
 
+// Прослушивание кликов кнопок
+button.forEach(btn => {
+  btn.addEventListener('click', handleClick);
+});
+
+// Функция создания боксов
+const setBoxes = function createBoxes(count) {
+  for (let i = 0; i < count; i += 1) {
+    boxes.insertAdjacentHTML("beforeend", `<div width=${startWidth} hight=${startHight} style=background-color:${getRandomHexColor()}></div>`);
+    startWidth += 10;
+    startHight += 10;
+  }
+};
+      
 // Функция удаления боксов
-const deleteBoxes = destroyBoxes() {
- 
+const deleteBoxes = function destroyBoxes() {
+  const count = boxes.children.length; // Определяет количество дочерних <div>
+  for (let k = 0; k < count; k += 1) {
+    boxes.firstChild.remove(); // Удаляет первый дочерний элемент
+  }
+          startWidth = 30;
+          startHight = 30;
 };
